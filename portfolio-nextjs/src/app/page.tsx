df20b4439442f02e57b4078b8c0eb16a646ab58b
MingKaiDev <47216870+MingKaiDev@users.noreply.github.com>
2025-09-17 15:01:43 +0800

    New Section Fix

--- portfolio-nextjs/src/app/page.tsx
+++ portfolio-nextjs/src/app/page.tsx
@@ -38,31 +38,32 @@ export default function Home() {
 
         <div className="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
           <div className="text-center md:text-left font-serif text-lg leading-relaxed">
-            <p className="mb-6">
-              I’m a <b>penultimate Computer Science student</b> at <b>Nanyang Technological University</b>, currently working at <b>DBS</b> as a <b>Site Reliability Engineering automation intern</b>. My primary focus is on optimizing system reliability, automating processes, and learning how to scale applications in the cloud.
-            </p>
-            <p className="mb-6">
-              Over the past <b>5+ years</b>, I’ve gained hands-on experience in a variety of areas within <b>Information Technology</b>, including:
-            </p>
-            <ul className="list-disc pl-8 mb-6">
-              <li><b>Front-end Development</b>: Building dynamic and user-friendly web applications using technologies like React, JavaScript, and HTML/CSS.</li>
-              <li><b>Back-end Development</b>: Working with databases, APIs, and server-side technologies such as Node.js and Python.</li>
-              <li><b>Automation</b>: Writing scripts and tools to automate repetitive tasks, which significantly reduces manual errors and improves efficiency.</li>
-              <li><b>Computer Vision</b>: Implementing image processing algorithms and machine learning models for visual data analysis.</li>
-              <li><b>Web Development</b>: Creating responsive, scalable, and interactive websites and apps that meet modern standards and user needs.</li>
-              <li><b>App UI/UX</b>: Focusing on designing intuitive and beautiful user interfaces for mobile and web applications.</li>
-              <li><b>Game Development</b>: Creating interactive gaming experiences using Unity, C#, and other game engines.</li>
-            </ul>
-            <p className="mb-6">
-              One of my <b>current ongoing projects</b> is a <b>Turn-Based tactical role-playing game</b> simulating <b>Fire Emblem</b>. This project has been an exciting challenge, as it involves a blend of game mechanics, artificial intelligence, and storytelling. It has allowed me to leverage many of the skills I’ve developed, from <b>game development</b> to <b>project management</b> and <b>collaborative teamwork</b>.
-            </p>
-            <p className="mb-6">
-              During my downtime, I’m passionate about exploring the culinary world, creating new dishes, and improving my skills in the kitchen. I also enjoy <b>mall-hopping</b>, <b>working out</b> in the gym, and, of course, playing games to unwind and stay connected with the latest trends in gaming.
-            </p>
-            <p className="mb-6">
-              I’m always seeking new challenges to improve my knowledge and skills. Whether it's <b>learning new technologies</b> or tackling <b>complex problems</b>, I am committed to becoming a versatile developer and <b>problem-solver</b>.
-            </p>
-          </div>
+  <p className="mb-6">
+    I&#39;m a <b>penultimate Computer Science student</b> at <b>Nanyang Technological University</b>, currently working at <b>DBS</b> as a <b>Site Reliability Engineering automation intern</b>. My primary focus is on optimizing system reliability, automating processes, and learning how to scale applications in the cloud.
+  </p>
+  <p className="mb-6">
+    Over the past <b>5+ years</b>, I&#39;ve gained hands-on experience in a variety of areas within <b>Information Technology</b>, including:
+  </p>
+  <ul className="list-disc pl-8 mb-6">
+    <li><b>Front-end Development</b>: Building dynamic and user-friendly web applications using technologies like React, JavaScript, and HTML/CSS.</li>
+    <li><b>Back-end Development</b>: Working with databases, APIs, and server-side technologies such as Node.js and Python.</li>
+    <li><b>Automation</b>: Writing scripts and tools to automate repetitive tasks, which significantly reduces manual errors and improves efficiency.</li>
+    <li><b>Computer Vision</b>: Implementing image processing algorithms and machine learning models for visual data analysis.</li>
+    <li><b>Web Development</b>: Creating responsive, scalable, and interactive websites and apps that meet modern standards and user needs.</li>
+    <li><b>App UI/UX</b>: Focusing on designing intuitive and beautiful user interfaces for mobile and web applications.</li>
+    <li><b>Game Development</b>: Creating interactive gaming experiences using Unity, C#, and other game engines.</li>
+  </ul>
+  <p className="mb-6">
+    One of my <b>current ongoing projects</b> is a <b>Turn-Based tactical role-playing game</b> simulating <b>Fire Emblem</b>. This project has been an exciting challenge, as it involves a blend of game mechanics, artificial intelligence, and storytelling. It has allowed me to leverage many of the skills I&#39;ve developed, from <b>game development</b> to <b>project management</b> and <b>collaborative teamwork</b>.
+  </p>
+  <p className="mb-6">
+    During my downtime, I&#39;m passionate about exploring the culinary world, creating new dishes, and improving my skills in the kitchen. I also enjoy <b>mall-hopping</b>, <b>working out</b> in the gym, and, of course, playing games to unwind and stay connected with the latest trends in gaming.
+  </p>
+  <p className="mb-6">
+    I&#39;m always seeking new challenges to improve my knowledge and skills. Whether it&#39;s <b>learning new technologies</b> or tackling <b>complex problems</b>, I am committed to becoming a versatile developer and <b>problem-solver</b>.
+  </p>
+</div>
+
 
           <img
             src="/images/potrait.png"
